6f19aac897c7f226450dcb44cb7c7d2cb85dab68
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2026-08-25 19:52:27 +0530

    bump version to 0.4.1

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -50,7 +50,7 @@ compose.desktop {
             appResourcesRootDir.set(project.layout.projectDirectory.dir("src/jvmMain/resources"))
             targetFormats(TargetFormat.Msi, TargetFormat.Deb, TargetFormat.AppImage, TargetFormat.Rpm)
             packageName = "PadConnectReceiver"
-            packageVersion = "0.4.0"
+            packageVersion = "0.4.1"
 
             windows {
                 console = true
