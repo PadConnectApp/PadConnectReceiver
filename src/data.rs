36f82b800b41cb082579945e67ac8f980cd5b8df
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2026-09-02 20:27:11 +0530

    add license headers

--- src/data.rs
+++ src/data.rs
@@ -1,3 +1,11 @@
+/*
+ * Copyright (C) 2026 Ishan
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, version 3 only.
+ *
+ * This program is distributed without any warranty. See the GNU General Public License for more details.
+ */
+
 #[derive(Debug, Default, Clone, Copy)]
 pub struct GamepadState {
     pub buttons: u16,
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+/*
+ * Copyright (C) 2026 Ishan
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, version 3 only.
+ *
+ * This program is distributed without any warranty. See the GNU General Public License for more details.
+ */
 
 mod data;
 mod input;
